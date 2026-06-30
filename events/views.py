5e9e1970b93293a8895132d081c94a16f3e590a5
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2026-06-30 22:06:15 +0530

    Add viewsets for events and reservations

--- events/views.py
+++ events/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Event, Reservation
+from .serializers import EventSerializer, ReservationSerializer
+
+class EventViewSet(viewsets.ModelViewSet):
+    serializer_class = EventSerializer
+
+    def get_queryset(self):
+        queryset = Event.objects.all()
+        status_param = self.request.query_params.get('status')
+        venue_param = self.request.query_params.get('venue')
+
+        if status_param:
+            queryset = queryset.filter(status=status_param)
+
+        if venue_param:
+            queryset = queryset.filter(venue__icontains= venue_param)
+        
+        return queryset
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    serializer_class = ReservationSerializer
+
+    def get_queryset(self):
+        queryset = Reservation.objects.all()
+
+        event_id = self.request.query_params.get('event_id')
+
+        if event_id:
+            queryset = queryset.filter(event_id=event_id)
+
+    
+    @action(detail=True, methods=['post'])
+    def cancel(self, request, pk=None):
+        reservation = self.get_object()
+
+        if reservation.status == 'cancelled':
+            return Response(
+                {'error': 'Already Cancelled'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        reservation.event.available_seats += reservation.seats_reservation
+        reservation.event.save()
+
+        return Response(self.get_serializer(reservation).data)
