a606971217e0c372b6912e9ce58cecbe0d8b6fd9
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2026-06-30 22:25:31 +0530

    added request logging middleware

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'events.middleware.RequestLoggingMiddleware'
 ]
 
 ROOT_URLCONF = 'eventhub.urls'
@@ -117,3 +118,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'
+        }
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO'
+    }
+}
